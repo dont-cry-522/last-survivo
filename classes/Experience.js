75c6cfc52e8d26a2f15889af76e328c4578a3273
dont-cry-522 <3445449877@qq.com>
2026-07-22 15:48:50 +0800

    fix: 升级检测改用 level 变化判断 (addExp 提前消耗经验导致永远检测不到)

--- classes/Experience.js
+++ classes/Experience.js
@@ -181,16 +181,14 @@ class ExperienceManager extends ObjectPool {
      */
     update(deltaTime, player, particleManager) {
         let leveledUp = false;
+        const prevLevel = player.level;
         for (let i = 0; i < this.pool.length; i++) {
             const orb = this.pool[i];
             if (!orb.active) continue;
-
-            const picked = orb.update(deltaTime, player, particleManager);
-            if (picked) {
-                if (player.exp >= player.expToNext) {
-                    leveledUp = true;
-                }
-            }
+            orb.update(deltaTime, player, particleManager);
+        }
+        if (player.level > prevLevel) {
+            leveledUp = true;
         }
         return leveledUp;
     }
